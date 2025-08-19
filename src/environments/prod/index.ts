505b40d49b186464a299e98e015b98b1a69dd83d
Danu Sabu <81710905+iamdanusabu@users.noreply.github.com>
2025-08-19 07:39:11 +0000

    Address changed

--- src/environments/prod/index.ts
+++ src/environments/prod/index.ts
@@ -1,6 +1,5 @@
-
 export const PROD_CONFIG = {
-  baseURL: "https://console.retailcloud.com",
+  baseURL: "https://betaaccount.retailcloud.com",
   grantType: "password",
   clientId: "rc-app-live",
   clientSecret: "secret",
